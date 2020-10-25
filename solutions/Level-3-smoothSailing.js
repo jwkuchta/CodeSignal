c5c97cbc7d6dda146dd2519e54275535fffe124e
Justina Kuchta <justyna.kuchta@gmail.com>
2020-10-25 18:34:38 -0400

    level 3 problem 2 solved

--- solutions/Level-3-smoothSailing.js
+++ solutions/Level-3-smoothSailing.js
@@ -14,3 +14,26 @@ function allLongestStrings(inputArray) {
     console.log(longest)
     return counter[longest]
 }
+
+// PROBLEM 2 SOLUTION
+
+function commonCharacterCount(s1, s2) {
+    let count = 0
+    let counter = {}
+    for (let char of s1) {
+      if (s2.includes(char)) {
+        if (!counter[char]) {
+          counter[char] = 1
+        } else {
+          counter[char] += 1
+        }
+      }
+    }
+    for (let char of s2) {
+      if (counter[char]) {
+        counter[char] -= 1
+        count++
+      }
+    }
+    return count
+}
